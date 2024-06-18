f20248dd8734743fc3f565847fd80b9a98e66ee0
ViniHGV <ixxvinicius@gmail.com>
2024-06-18 20:13:46 -0300

    feat: adicionando JwtService no modulo

--- src/tools/tools.module.ts
+++ src/tools/tools.module.ts
@@ -3,9 +3,10 @@ import { ToolsService } from './tools.service';
 import { ToolsController } from './tools.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaClient } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [ToolsController],
-  providers: [ToolsService, PrismaService, PrismaClient],
+  providers: [ToolsService, PrismaService, PrismaClient, JwtService],
 })
 export class ToolsModule {}
